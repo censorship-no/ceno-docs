c0696fe0c2c1bdded9829989fac4207ba74c845c
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2020-04-08 13:20:27 -0400

    Add Emacs directory variables for User Manual.

---
+++ user-manual/.dir-locals.el
@@ -0,0 +1,10 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((markdown-mode
+  (eval . (visual-fill-column-mode))
+  (eval . (visual-line-mode))
+  (eval . (flyspell-mode)))
+ ("en"
+  (markdown-mode
+   (ispell-local-dictionary . "american"))))
