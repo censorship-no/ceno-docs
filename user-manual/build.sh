896d3f04c0f77d38bb341931191bc3322521c52b
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2020-04-24 17:19:47 -0400

    Build `xx.toml` book into `xx.book` (via mdBook config).

--- user-manual/build.sh
+++ user-manual/build.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
+# Usage: build.sh LANGUAGE
 set -e
+test -f "$1.toml"
+conf="$1.toml"
+test -L book.toml -o ! -e book.toml
+ln -sf "$conf" book.toml
 mdbook build
 # [Remove Google surveillance](https://github.com/rust-lang/mdBook/issues/847).
-find book -name '*.html' | xargs sed -i '/<link href="[^"]*\bfonts\.googleapis\.com\b/d'
+outd=$(sed -En 's/^build-dir *= *"(.*)"/\1/p' "$conf")
+find "$outd" -name '*.html' | xargs sed -i '/<link href="[^"]*\bfonts\.googleapis\.com\b/d'
