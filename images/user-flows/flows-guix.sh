42e667c1e000c1928e74d45c5b689f32727b8006
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2020-06-03 14:04:26 -0400

    User Manual: Allow editing with flows diagram env generation script.

--- images/user-flows/flows-guix.sh
+++ images/user-flows/flows-guix.sh
@@ -1,8 +1,21 @@
 #!/bin/sh
 
-FLOWS_SCRIPT="$(dirname "$(realpath "$0")")/flows.sh"
+# Usage: flows-guix.sh --edit
+#    or: flows-guix.sh [OUTPUT_PREFIX]
 
+FLOWS_DIR="$(dirname "$(realpath "$0")")"
+
+guix_env() {
 guix time-machine --commit=b5c2d93d7a223155898dd0ed6932f6acf78ac454 \
-     -- environment -CP --pure --expose=$(realpath $(dirname $FLOWS_SCRIPT)) \
+     -- environment -CP --pure \
      --ad-hoc coreutils dash dia font-dejavu font-gnu-unifont font-gnu-freefont-ttf fontconfig grep sed \
-     -- dash -c "fc-cache && dash $FLOWS_SCRIPT $@"
+     "$@"
+}
+
+if [ "$1" = "--edit" ]; then
+    guix_env --share=$FLOWS_DIR --expose=/tmp/.X11-unix \
+             -- env DISPLAY="$DISPLAY" dia "$FLOWS_DIR/flows.dia"
+else
+    guix_env --expose=$FLOWS_DIR \
+             -- dash -c "fc-cache && dash $FLOWS_DIR/flows.sh $@"
+fi
