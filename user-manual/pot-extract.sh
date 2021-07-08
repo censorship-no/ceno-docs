2bd5009c1ae19c31cc95502809c7224e3e6f40a6
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2021-07-08 16:51:28 +0200

    Lower-case Markdown file names when computing POT file names.
    To follow current naming (we may drop this later on).

--- user-manual/pot-extract.sh
+++ user-manual/pot-extract.sh
@@ -8,7 +8,8 @@ cd en
 POT_DIR=../en.pot
 
 for md in $(find . -name '*.md'); do
-    pot="$POT_DIR/${md%%.md}.pot"
+    #pot="$POT_DIR/${md%%.md}.pot"  # use this if file names are kept
+    pot="$POT_DIR/$(dirname "$md")/$(echo "$(basename "${md%%.md}.pot" | tr [:upper:] [:lower:])")"
     mkdir -p "$(dirname "$pot")"
     md2po -d 'Content-Type: text/plain; charset=utf-8' "$md" | msgattrib --clear-fuzzy --empty - > "$pot"
 done
