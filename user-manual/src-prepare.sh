7ee5d143575d18278da01d9fa1f2247e145ee4e9
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2021-07-08 14:02:13 +0200

    Copy images into generated Manual sources.

--- user-manual/src-prepare.sh
+++ user-manual/src-prepare.sh
@@ -7,9 +7,26 @@ cd "$1"
 DEST="../$1.src"
 
 mkdir "$DEST"  # fail if existing
+
+# Generate Markdown files from PO translations.
 for po in $(find . -name '*.po'); do
     md="$(dirname "$po")/$(echo "$(basename "${po%%.po}" | sed -e 's/readme/README/' -e 's/summary/SUMMARY/').md")"
     mkdir -p "$(dirname "$DEST/$md")"
     po2md -q -w 100000 -p "$po" -s "$DEST/$md" "../en/$md"
 done
-# TODO: Copy images.
+
+# Copy translated images.
+for img in $(find . -name '*.png' -o -name '*.svg'); do
+    imgd="$(dirname "$DEST/$img")"
+    mkdir -p "$imgd"
+    cp "$img" "$imgd"
+done
+
+# Copy non-translated images.
+cd ../en
+for img in $(find . -name '*.png' -o -name '*.svg'); do
+    imgd="$(dirname "$DEST/$img")"
+    if test -f "$imgd/$(basename "$img")"; then continue; fi
+    mkdir -p "$imgd"
+    cp "$img" "$imgd"
+done
