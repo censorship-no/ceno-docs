db12e099dcbf5b6f1e52c8908af36d3788e14321
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2021-07-19 12:12:57 +0200

    User Manual: Workaround for RTL output.
    Util mdBook natively supports it.
    
    Also make Persian use the workaround.

--- user-manual/build.sh
+++ user-manual/build.sh
@@ -12,3 +12,9 @@ mdbook build
 # No longer needed with mdBook v0.40.0.
 #outd=$(sed -En 's/^build-dir *= *"(.*)"/\1/p' "$conf")
 #find "$outd" -name '*.html' | xargs sed -i '/<link href="[^"]*\bfonts\.googleapis\.com\b/d'
+
+# Fix output for RTL languages (needs <https://github.com/rust-lang/mdBook/pull/1489>).
+if grep -qE '^_direction\s*=\s*"?rtl?"' "$conf"; then
+    book="$(sed -nE 's/^build-dir\s*=\s"([^"]+)".*/\1/p' "$conf")"
+    find "$book" -name '*.html' -exec sed -i 's/<body /<body dir="rtl" /' '{}' ';'
+fi
